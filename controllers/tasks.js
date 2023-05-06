3cb8cc8c4e9efcdc92f9749007983361c77b8d94
Prafull Epili <prafullepili@gmail.com>
2023-05-06 06:54:59 +0530

    getTask,deleteTask completed

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,14 +18,37 @@ const createTask = async (req, res) => {
         res.status(500).json(error)
     }
 }
-const getTask = (req, res) => {
+
+
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOne({ _id: taskID })
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id : ${taskID}` })
+        }
+        return res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json(error)
+    }
     res.send('get single task');
 }
+
+
 const updateTask = (req, res) => {
     res.send('update task');
 }
-const deleteTask = (req, res) => {
-    res.send('delete task');
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndDelete({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id ${task}` })
+        }
+        res.status(200).json({ task })
+    } catch (error) {
+        return res.status(404).json(error)
+    }
 }
 
 module.exports = {
